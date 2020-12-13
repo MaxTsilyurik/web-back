cb32030b13d909bac980fd128d84e0e182c99dc0
Цилюрик Максим Андреевич <tsilyurik.ma@reliab.tech>
2020-12-13 16:29:11 +0300

    feat: add @RestController

--- src/main/java/com/example/webback/web/api/v1/FriendsController.java
+++ src/main/java/com/example/webback/web/api/v1/FriendsController.java
@@ -1,9 +1,11 @@
 package com.example.webback.web.api.v1;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import static com.example.webback.business.utils.ApiConstant.FRIENDS;
 
 @RequestMapping(FRIENDS)
+@RestController
 public class FriendsController {
 }
