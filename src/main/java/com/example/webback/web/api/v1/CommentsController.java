08168a96ebae4c4d10a145356f7e852b33341c83
Цилюрик Максим Андреевич <tsilyurik.ma@reliab.tech>
2020-12-18 23:08:21 +0300

    дабавлен CommentsController

---
+++ src/main/java/com/example/webback/web/api/v1/CommentsController.java
@@ -0,0 +1,11 @@
+package com.example.webback.web.api.v1;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static com.example.webback.business.utils.ApiConstant.COMMENTS;
+
+@RequestMapping(COMMENTS)
+@RestController
+public class CommentsController {
+}
